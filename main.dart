ce59747e72b2fabfe6e6d541c9d7ca8e7b2690e3
Egemberdievariet <57054923+egemberdievariet@users.noreply.github.com>
2021-10-27 19:35:54 +0600

    change

--- main.dart
+++ main.dart
@@ -1,15 +1,17 @@
 main(List<String> args) {
   print(task1(String));
-  print(task2(3, 4));
+  print(task2(null, null));
 }
 
-String task1(String) {}
+String? task1(String) {}
 
-int? task2(int a, int b) {
+int task2(int? a, int? b) {
   if (a == null) {
     a == 1;
   } else if (b == null) {
     b == 2;
+  } else {
+    return a + b;
   }
-  return a + b;
+  return a! + b!;
 }
